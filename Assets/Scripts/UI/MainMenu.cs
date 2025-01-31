95989867e4d5f6802ec8e5e5a3168a3575c549d5
popoyo07 <154477545+popoyo07@users.noreply.github.com>
2025-01-30 19:20:07 -0500

    Main menu and credits update

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -6,17 +6,26 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    // Send player to Level 1
     public void StartGame()
     {
         SceneManager.LoadScene("Level 1");
     }
+    // Send to Credit scne 
     public void LoadCredit()
     {
         SceneManager.LoadScene("Credits");
     }
+
+    // Quit the game  
     public void LoadQuit()
     {
         Application.Quit();
+    }
+    
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
     }
 
 }
