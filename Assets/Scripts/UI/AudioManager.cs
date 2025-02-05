270500f03d31487e36e2c5770cc05a12bbb3ce5d
Sze <sz982600@ucf.edu>
2025-02-05 16:22:32 -0500

    Update AudioManager.cs

--- Assets/Scripts/UI/AudioManager.cs
+++ Assets/Scripts/UI/AudioManager.cs
@@ -1,23 +1,76 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    [SerializeField] AudioSource SFXSource;
+    public static AudioManager instance;
 
+    [Header("Audio Sources")]
+    public AudioSource bgmSource;
+    public AudioSource sfxSource;
+
+    [Header("Audio Clips")]
+    public AudioClip bgm;
+    public AudioClip attackbgm;
     public AudioClip jumping;
-    public AudioClip death;
     public AudioClip getCoin;
+    public AudioClip death;
+
+    private float bgmResumeTime = 0f;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 
     private void Start()
     {
-        SFXSource = GetComponent<AudioSource>();
-        SFXSource.volume = 0.5f;
+        bgmSource.volume = 0.3f;
+        PlayBGM(bgm);
     }
 
+    // Play normal BGM and resume from where it left off
+    public void ResumeBGM()
+    {
+        if (bgmSource.clip == bgm)
+        {
+            bgmSource.time = bgmResumeTime;
+            bgmSource.Play();
+        }
+
+    }
+
+    // Pause BGM and store its playback time
+    public void PauseBGM()
+    {
+        if (bgmSource.isPlaying)
+        {
+            bgmResumeTime = bgmSource.time;
+            bgmSource.Pause();
+        }
+    }
+
+    // Play a looping background music track
+    public void PlayBGM(AudioClip bgmClip)
+    {
+        if (bgmSource.clip != bgmClip)
+        {
+            bgmSource.clip = bgmClip;
+            bgmSource.loop = true;
+            bgmSource.Play();
+        }
+    }
+
+    // Play a short sound effect
     public void PlaySFX(AudioClip clip)
     {
-        SFXSource.PlayOneShot(clip);
+        sfxSource.PlayOneShot(clip);
     }
 }
