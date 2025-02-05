869535b56dfc1a0c0d8ef5d9d62ca3b98a1c2d33
kttnguyen <ki317603@ucf.edu>
2025-02-05 02:45:51 -0500

    added finalScore

--- Assets/Scripts/FinalScore.cs
+++ Assets/Scripts/FinalScore.cs
@@ -1,9 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
-public class FinalScore : MonoBehaviour
+public class ScoreDisplay : MonoBehaviour
 {
-   public GameManager gamemanager;
-   
+    public TextMeshProUGUI scoreText;
+
+    void Start()
+    {
+        int savedScore = PlayerPrefs.GetInt("SavedScore", 0);
+        UpdateScore(savedScore);
+    }
+
+    public void UpdateScore(int newScore)
+    {
+     scoreText.text = "Score: \n" + newScore;
+    }
 }
