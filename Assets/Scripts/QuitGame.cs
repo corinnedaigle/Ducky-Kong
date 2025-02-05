b33f3633c968204d0482bacb8b5bf762835b39af
popoyo07 <piwedaa@gmail.com>
2025-02-05 02:48:39 -0500

    changed how camera works a bit

--- Assets/Scripts/QuitGame.cs
+++ Assets/Scripts/QuitGame.cs
@@ -6,14 +6,4 @@ public class QuitGame : MonoBehaviour
 {
 
 
-    // Update is called once per frame
-    void Update()
-    {
-        // esc to end game 
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            Application.Quit();
-            Debug.Log("quit aplication");
-        }
-    }
 }
