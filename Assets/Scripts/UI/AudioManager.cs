01ae76675d26efc61157e961d366ae0056a92279
Sze <sz982600@ucf.edu>
2025-02-05 01:33:13 -0500

    Updated Audio

--- Assets/Scripts/UI/AudioManager.cs
+++ Assets/Scripts/UI/AudioManager.cs
@@ -1,36 +1,76 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    [SerializeField] AudioSource SFXSource;
-    [SerializeField] AudioSource BGMSource;
+    public static AudioManager instance;
 
-    public AudioClip jumping;
-    public AudioClip death;
-    public AudioClip getCoin;
+    [Header("Audio Sources")]
+    public AudioSource bgmSource;
+    public AudioSource sfxSource;
 
-    public AudioClip attackbgm;
+    [Header("Audio Clips")]
     public AudioClip bgm;
+    public AudioClip attackbgm;
+    public AudioClip jumping;
+    public AudioClip getCoin;
+    public AudioClip death;
 
+    private float bgmResumeTime = 0f;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 
     private void Start()
     {
-        SFXSource = GetComponent<AudioSource>();
-        SFXSource.volume = 0.5f;
-
-        BGMSource = GetComponent<AudioSource>();
-        SFXSource.volume = 0.271f;
+        bgmSource.volume = 0.3f;
+        PlayBGM(bgm);
     }
 
+    // Play normal BGM and resume from where it left off
+    public void ResumeBGM()
+    {
+        if (bgmSource.clip == bgm)
+        {
+            bgmSource.time = bgmResumeTime;
+            bgmSource.Play();
+        }
+
+    }
+
+    // Pause BGM and store its playback time
+    public void PauseBGM()
+    {
+        if (bgmSource.isPlaying)
+        {
+            bgmResumeTime = bgmSource.time;
+            bgmSource.Pause();
+        }
+    }
+
+    // Play a looping background music track
+    public void PlayBGM(AudioClip bgmClip)
+    {
+        if (bgmSource.clip != bgmClip)
+        {
+            bgmSource.clip = bgmClip;
+            bgmSource.loop = true;
+            bgmSource.Play();
+        }
+    }
+
+    // Play a short sound effect
     public void PlaySFX(AudioClip clip)
     {
-        SFXSource.PlayOneShot(clip);
-    }
-
-    public void PlayBGM(AudioClip clip)
-    {
-        BGMSource.PlayOneShot(clip);
+        sfxSource.PlayOneShot(clip);
     }
 }
